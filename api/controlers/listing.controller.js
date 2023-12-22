67b343753d58d8966d878a48270bdd6eba94f76a
JyotiSingh65 <2021ucp1374@mnit.ac.in>
2023-12-22 23:53:09 +0530

    completed delete user listing functionality

--- api/controlers/listing.controller.js
+++ api/controlers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next ) => {
 
@@ -11,4 +12,29 @@ export const createListing = async (req, res, next ) => {
         next(err);
     }
 
+};
+
+export const deleteListing = async (req, res, next ) => {
+
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errorHandler(404,"Listing not found")  ) ;
+    }
+
+    if(req.user.id !== listing.userRef) {
+
+        return next(errorHandler(401,"You are not authorized to delete this listing" )  ) ;
+    
+    }
+
+    try{
+
+        await Listing.findByIdAndDelete(req.params.id);
+        return res.status(200).json({message:"Listing deleted successfully"});
+        
+
+    } catch(err) {
+        next(err);
+    }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -156,6 +156,30 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+
+    try {
+
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      
+      });
+
+      const data = await res.json();
+      if(data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+
+
+    } catch (error) {
+       console.log(error);
+    }
+
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -208,7 +232,7 @@ export default function Profile() {
               <p className=''>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
                 <button className="text-green-700 uppercase">Edit</button>
                 
               </div>
@@ -217,8 +241,6 @@ export default function Profile() {
         ))}
         </div>
 
-        
-      
       }
     </div>
   )
