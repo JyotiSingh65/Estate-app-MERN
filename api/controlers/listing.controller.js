909430da0914f3faaccfe125f453df16ce14c6dc
JyotiSingh65 <2021ucp1374@mnit.ac.in>
2023-12-23 00:21:56 +0530

    Create update listing Api route

--- api/controlers/listing.controller.js
+++ api/controlers/listing.controller.js
@@ -38,3 +38,30 @@ export const deleteListing = async (req, res, next ) => {
         next(err);
     }
 };
+
+export const updateListing = async (req, res, next ) => {
+         
+        const listing = await Listing.findById(req.params.id);
+        if(!listing) {
+            return next(errorHandler(404,"Listing not found")  ) ;
+        }
+
+        if(req.user.id !== listing.userRef) {
+            return next(errorHandler(401,"You are not authorized to update this listing" )  ) ;
+        }
+
+        try {
+
+            const updatedListing = await Listing.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {new:true}
+            );
+
+            return res.status(200).json(updatedListing);
+            
+
+        } catch(err) {
+            next(err);
+        }
+};
