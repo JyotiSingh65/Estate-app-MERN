5099233012e11dca75ad5d7f43d2998befab525f
JyotiSingh65 <2021ucp1374@mnit.ac.in>
2023-12-25 19:04:10 +0530

    Add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='3-xl font-bold mb-4 text-slate-800'>About Our Estates</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi in consectetur a commodi quia distinctio fugiat, sint sequi perspiciatis quas voluptatum cum cumque impedit molestiae amet magni voluptatem culpa autem totam nam sed quisquam iusto magnam. Obcaecati officia, ad quam necessitatibus quo nobis officiis eaque iusto itaque error corrupti natus.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit itaque ratione atque unde omnis, eos voluptate iusto architecto iure consectetur quisquam, quibusdam distinctio eaque dolor voluptatem vel corporis provident accusamus aspernatur error maiores. Rem at quod adipisci! Quibusdam facere hic repellat pariatur sint totam eveniet sunt modi, nulla cum excepturi, voluptatum aliquid, nisi nemo nam alias! Impedit voluptatibus aperiam veritatis ducimus dolores vitae, nam ratione laudantium odit, nihil delectus cum beatae dignissimos optio obcaecati autem omnis tenetur, vel temporibus ex?</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, placeat debitis. Obcaecati, ducimus distinctio laborum praesentium aliquam excepturi incidunt. Rerum ea libero unde voluptate aut error velit odio molestias inventore iure. Cupiditate dolore aperiam, laboriosam laudantium quia pariatur provident quo veniam dolor unde dolores tempora mollitia corporis est consequuntur fugit voluptas odit ducimus? Ipsum accusamus dolorem eum! Repellendus, obcaecati, quibusdam dolorum, totam esse quia odio ut neque fuga nobis sunt.</p>
     </div>
   )
 }
